9e2270ad08d0ca5b70a96320b001399927178654
Nageshwar Prajapati <nageshwarprajapati787@gmail.com>
2025-03-31 09:11:32 +0000

    que2

--- JavaQuestion/NormlQues.java
+++ JavaQuestion/NormlQues.java
@@ -5,5 +5,106 @@ import java.util.Scanner;
 public class NormlQues {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        
+        //Que:-16 WAP Which intacks 6 number as input and print their average
+        // System.out.print("numbar a:-");
+        // double a=sc.nextDouble();
+        // System.out.print("numbar b:-");
+        // double b=sc.nextDouble();
+        // System.out.print("numbar c:-");
+        // double c=sc.nextDouble();
+        // System.out.print("numbar d:-");
+        // double d=sc.nextDouble();
+        // System.out.print("numbar e:-");
+        // double e=sc.nextDouble();
+        // System.out.print("numbar f:-");
+        // double f=sc.nextDouble();
+        // double average=(a+b+c+d+f)/6;
+        // System.out.println("Average:-"+average);
+
+        //Que:-17 WAP to Print sum Of all even numbar form 1 to 10
+        // int sum=0;
+        // for(int i=0;i<=10;i++){
+        //     if(i%2==0){
+        //         sum=sum+i;
+        //     }
+        // }
+        // System.out.println("sum of all even no is:-"+sum);
+
+        // Que:18 WAP to print rectangle of area  take the input for user.
+        // System.out.print("Enter the width:-");
+        // double width=sc.nextDouble();
+        // System.out.print("Enter the height:-");
+        // double height=sc.nextDouble();
+        // System.out.println("Area of Rectangle is:-"+(width*height));
+
+        // Que:-19 swap two number take the user input using third variable.
+        // System.out.print("Enter the first number:-");
+        // int a=sc.nextInt();
+        // System.out.print("Enter the second number:-");
+        // int b=sc.nextInt();
+        // int temp;
+        // System.out.println("before swap");
+        // System.out.println(a);
+        // System.out.println(b);
+        // temp=a;
+        // a=b;
+        // b=temp;
+        // System.out.println("after swap");
+        // System.out.println(a);
+        // System.out.println(b);
+
+         // Que:-19 swap two number take the user input using two variable.
+        //  System.out.print("Enter the first number:-");
+        //  int a=sc.nextInt();
+        //  System.out.print("Enter the second number:-");
+        //  int b=sc.nextInt();
+        
+        //  System.out.println("before swap");
+        //  System.out.println(a);
+        //  System.out.println(b);
+
+        //  a=a+b;
+        //  b=a-b;
+        //  a=a-b;
+        //  System.out.println("after swap");
+        //  System.out.println(a);
+        //  System.out.println(b);
+
+        // Que:-20 WAP to intack width & height of a rectangle and find its perimeter.
+        // System.out.print("Enter the width:-");
+        // double width=sc.nextDouble();
+        // System.out.print("Enter the height:-");
+        // double height=sc.nextDouble();
+        // System.out.println("Perimeter of Rectangle is:-"+2*(width+height));
+
+
+        //Que:-21 WAP to find the sum of all the digits in the input number
+        System.out.print("Enter the first number:-");
+        // int num=sc.nextInt();
+        // int sum=0;
+        // while (num !=0) {
+        //     sum=sum+num%10;
+        //     num=num/10;
+        // }
+        // System.out.println("sum of all digits the given number is:-"+sum);
+
+        // Que:- 22 WAP to convert a string to integer
+        // String str="9";
+        // int num=Integer.parseInt(str);
+        // System.out.println(num+1);
+
+        // Que:-23 WAP to convert a string to integer
+        // int num=555;
+        // String str=Integer.toString(num);
+        // System.out.println(str+1);
+
+
+
+
+
+
+
+
     }
 }
